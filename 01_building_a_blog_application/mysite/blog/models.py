3dc84b18f56107358164b0f1c2c03f3f37d48ffe
Pawel Dabala <pdabala@gmail.com>
2020-05-06 14:28:46 +0200

    extend model Post

--- 01_building_a_blog_application/mysite/blog/models.py
+++ 01_building_a_blog_application/mysite/blog/models.py
@@ -17,4 +17,14 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
-    created = models.DateTimeField
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=False)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+
+    class Meta:
+        ordering = ('-publish',)
+
+    def __str__(self):
+        return self.title
+
+        
